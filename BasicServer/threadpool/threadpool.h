f14453d901e5a91e9ee298f41d077c29ff2c2783
Alex <38066642+alexuyz@users.noreply.github.com>
2025-03-14 03:22:41 -0700

    Update threadpool.h

--- BasicServer/threadpool/threadpool.h
+++ BasicServer/threadpool/threadpool.h
@@ -1,152 +1,151 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <list>
-#include <cstdio>
-#include <exception>
 #include <pthread.h>
-#include "../lock/locker.h"
-#include "../CGImysql/sql_connection_pool.h"
+#include <list>
+#include <exception>
+#include <cstdio>
+#include <semaphore.h>
 
-template <typename T>
-class threadpool
-{
+// Thread pool class, defined as a template class for code reuse
+template<typename T>
+class ThreadPool {
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
-    threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
-    ~threadpool();
-    bool append(T *request, int state);
-    bool append_p(T *request);
+    // thread_number is the number of threads in the thread pool
+    // max_requests is the maximum number of requests allowed in the request queue waiting for processing
+    ThreadPool(int thread_number = 8, int max_requests = 10000);
+    ~ThreadPool();
+    
+    // Add a task to the request queue
+    bool append(T* request);
 
 private:
-    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
-    static void *worker(void *arg);
+    // Function run by worker threads, continuously taking tasks from the work queue and executing them
+    static void* worker(void* arg);
     void run();
 
 private:
-    int m_thread_number;        //线程池中的线程数
-    int m_max_requests;         //请求队列中允许的最大请求数
-    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
-    std::list<T *> m_workqueue; //请求队列
-    locker m_queuelocker;       //保护请求队列的互斥锁
-    sem m_queuestat;            //是否有任务需要处理
-    connection_pool *m_connPool;  //数据库
-    int m_actor_model;          //模型切换
+    // Number of threads
+    int m_thread_number;
+    
+    // Array describing the thread pool, size is m_thread_number
+    pthread_t* m_threads;
+    
+    // Maximum number of requests allowed in the request queue waiting for processing
+    int m_max_requests;
+    
+    // Request queue
+    std::list<T*> m_workqueue;
+    
+    // Mutex lock protecting the request queue
+    pthread_mutex_t m_queuelocker;
+    
+    // Semaphore to indicate if there are tasks to process
+    sem_t m_queuestat;
+    
+    // Flag to end threads
+    bool m_stop;
 };
-template <typename T>
-threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
-{
-    if (thread_number <= 0 || max_requests <= 0)
+
+template<typename T>
+ThreadPool<T>::ThreadPool(int thread_number, int max_requests) : 
+    m_thread_number(thread_number), m_max_requests(max_requests), 
+    m_stop(false), m_threads(NULL) {
+    
+    if((thread_number <= 0) || (max_requests <= 0)) {
         throw std::exception();
+    }
+    
     m_threads = new pthread_t[m_thread_number];
-    if (!m_threads)
+    if(!m_threads) {
         throw std::exception();
-    for (int i = 0; i < thread_number; ++i)
-    {
-        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
-        {
-            delete[] m_threads;
+    }
+    
+    // Create thread_number threads and set them as detached threads
+    for(int i = 0; i < thread_number; ++i) {
+        printf("create the %dth thread\n", i);
+        
+        if(pthread_create(m_threads + i, NULL, worker, this) != 0) {
+            delete [] m_threads;
             throw std::exception();
         }
-        if (pthread_detach(m_threads[i]))
-        {
-            delete[] m_threads;
+        
+        if(pthread_detach(m_threads[i])) {
+            delete [] m_threads;
             throw std::exception();
         }
     }
+    
+    // Initialize mutex and semaphore
+    if(pthread_mutex_init(&m_queuelocker, NULL) != 0) {
+        delete [] m_threads;
+        throw std::exception();
+    }
+    
+    if(sem_init(&m_queuestat, 0, 0) != 0) {
+        delete [] m_threads;
+        pthread_mutex_destroy(&m_queuelocker);
+        throw std::exception();
+    }
 }
-template <typename T>
-threadpool<T>::~threadpool()
-{
-    delete[] m_threads;
+
+template<typename T>
+ThreadPool<T>::~ThreadPool() {
+    m_stop = true;
+    delete [] m_threads;
+    pthread_mutex_destroy(&m_queuelocker);
+    sem_destroy(&m_queuestat);
 }
-template <typename T>
-bool threadpool<T>::append(T *request, int state)
-{
-    m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
-        m_queuelocker.unlock();
+
+template<typename T>
+bool ThreadPool<T>::append(T* request) {
+    // Always lock when operating on the work queue, as it's shared by all threads
+    pthread_mutex_lock(&m_queuelocker);
+    if(m_workqueue.size() > m_max_requests) {
+        pthread_mutex_unlock(&m_queuelocker);
         return false;
     }
-    request->m_state = state;
+    
     m_workqueue.push_back(request);
-    m_queuelocker.unlock();
-    m_queuestat.post();
+    pthread_mutex_unlock(&m_queuelocker);
+    
+    // Increase semaphore, notify worker threads that a new task has arrived
+    sem_post(&m_queuestat);
     return true;
 }
-template <typename T>
-bool threadpool<T>::append_p(T *request)
-{
-    m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
-    {
-        m_queuelocker.unlock();
-        return false;
-    }
-    m_workqueue.push_back(request);
-    m_queuelocker.unlock();
-    m_queuestat.post();
-    return true;
-}
-template <typename T>
-void *threadpool<T>::worker(void *arg)
-{
-    threadpool *pool = (threadpool *)arg;
+
+template<typename T>
+void* ThreadPool<T>::worker(void* arg) {
+    ThreadPool* pool = (ThreadPool*)arg;
     pool->run();
     return pool;
 }
-template <typename T>
-void threadpool<T>::run()
-{
-    while (true)
-    {
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if (m_workqueue.empty())
-        {
-            m_queuelocker.unlock();
+
+template<typename T>
+void ThreadPool<T>::run() {
+    while(!m_stop) {
+        // Wait for semaphore, wake up when there's a task
+        sem_wait(&m_queuestat);
+        
+        // Acquire mutex lock
+        pthread_mutex_lock(&m_queuelocker);
+        if(m_workqueue.empty()) {
+            pthread_mutex_unlock(&m_queuelocker);
             continue;
         }
-        T *request = m_workqueue.front();
+        
+        // Take the first task from the queue
+        T* request = m_workqueue.front();
         m_workqueue.pop_front();
-        m_queuelocker.unlock();
-        if (!request)
+        pthread_mutex_unlock(&m_queuelocker);
+        
+        if(!request) {
             continue;
-        if (1 == m_actor_model)
-        {
-            if (0 == request->m_state)
-            {
-                if (request->read_once())
-                {
-                    request->improv = 1;
-                    connectionRAII mysqlcon(&request->mysql, m_connPool);
-                    request->process();
-                }
-                else
-                {
-                    request->improv = 1;
-                    request->timer_flag = 1;
-                }
-            }
-            else
-            {
-                if (request->write())
-                {
-                    request->improv = 1;
-                }
-                else
-                {
-                    request->improv = 1;
-                    request->timer_flag = 1;
-                }
-            }
         }
-        else
-        {
-            connectionRAII mysqlcon(&request->mysql, m_connPool);
-            request->process();
-        }
+        
+        // Execute the task
+        request->process();
     }
 }
+
 #endif
